a5adf78831872861d40b07acd4cbd3959041a892
Vashti259 <156109212+Vashti259@users.noreply.github.com>
2024-02-17 12:39:30 -1100

    vashti-07.py

--- vashti-07.py
+++ vashti-07.py
@@ -1,1 +1,29 @@
+Datafun-07-ml
+Vashti Gambol
+February 17th, 2024
 
+##  Overview
+This is Module 7 where we acknowledge class and regression working in Python.  Python is an object-orientated programming language and we will also add SQL which is a (structured query language) perfect for relational data, to build this model in module 7.
+
+## Objective
+Build   Model,
+Test    Model,
+Explore Model
+
+## Environment Setup
+Clone the current repository to the local machine using git clone https://github.com/Vashti259/Datafun-07-ml
+and execute the work in VSCde. Create a virtual environment in the local machine root folder in VSCode. 
+This new folder is called .venv which will have a .gitignore file to to manage the repository/project.
+Windows terminal installs vinv py -m venv.venv
+Activate venv virtual environment ./.venv/script/activate.ps1
+
+## Dependencies for Project terminal in VSCode
+Add Python tools to build the project
+** import pandas as pd
+import numpy as np
+Import matplotlib as plt
+Import Seaborn as sns
+
+## Freeze Dependencies
+others can recreate this project on their machine
+py -m pip freeze>requirement.txt
